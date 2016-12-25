663c17c45204b62d5ad1c733963e5c8ac6b96956
muyanbiao <muyanbiao@gmail.com>
2016-12-25 22:03:43 +0800

    打印INT32_MAX, INT32_MIN的值

---
+++ chap02/sample2-2-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* the min,max value of signed and unsigned char, int, long, double  */
+int main()
+{
+	printf("int32_max = %d\n", INT32_MAX);
+	printf("int32_min = %d\n", INT32_MIN);
+	printf("uint32_max = %d\n", UINT32_MAX);
+	//printf("uint32_min = %d\n", UINT32_MIN);
+
+	return 0;
+}
